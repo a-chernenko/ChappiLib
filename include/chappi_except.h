beee5773d90d42f983e617253fff705614f388b3
Alexander Chernenko <chernenko.a@insys.ru>
2021-12-17 18:50:20 +0300

    Refactoring

--- include/chappi_except.h
+++ include/chappi_except.h
@@ -31,15 +31,21 @@ namespace chappi {
 
 template <typename ErrorType>
 class runtime_error : public std::runtime_error {
-  ErrorType _error;
+    ErrorType _error;
 
- public:
-  runtime_error(ErrorType error, const char *message)
-      : std::runtime_error(message), _error{error} {}
-  runtime_error(ErrorType error, std::string message)
-      : std::runtime_error(message), _error{error} {}
-  ErrorType get_error() const noexcept { return _error; }
-  virtual ~runtime_error() noexcept = default;
+public:
+    runtime_error(ErrorType error, const char* message)
+        : std::runtime_error(message)
+        , _error { error }
+    {
+    }
+    runtime_error(ErrorType error, std::string message)
+        : std::runtime_error(message)
+        , _error { error }
+    {
+    }
+    ErrorType get_error() const noexcept { return _error; }
+    virtual ~runtime_error() noexcept = default;
 };
 
-}  // namespace chappi
+} // namespace chappi
